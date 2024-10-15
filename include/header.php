fbeb9a5ef722dfbbf0884fafd118930a1a45ed1a
LEVANNHAT <hiamnhatdz203@gmail.com>
2024-10-15 14:14:15 +0700

    deploy web l3

--- include/header.php
+++ include/header.php
@@ -12,7 +12,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/solid.min.css">
-    <base href="http://localhost/qlvn/">
+    <base href="http://13.229.107.110/qlnv/">
+    <!-- <base href="http://localhost/qlvn/"> -->
     <link rel="stylesheet" href="css/style.css">
     <title>Admintrator</title>
 </head>
