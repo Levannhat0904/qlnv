766559a09e06359494fd851dc226cec49d134501
LEVANNHAT <hiamnhatdz203@gmail.com>
2024-10-28 14:15:54 +0700

    update l11

--- database/db_helper.php
+++ database/db_helper.php
@@ -11,10 +11,10 @@
 // $dbname = "qlnv"; // Tên cơ sở dữ liệu
 // db_hiep
 
-// $servername = "database-1.cp80m0wgc1ht.ap-southeast-1.rds.amazonaws.com"; // Địa chỉ máy chủ MySQL
-// $username = "admin"; // Tên đăng nhập MySQL
-// $password = "Levannhat0904?"; // Mật khẩu MySQL
-// $dbname = "qlnv"; // Tên cơ sở dữ liệu
+$servername = "database-1.cxuy0au02kfl.ap-southeast-1.rds.amazonaws.com"; // Địa chỉ máy chủ MySQL
+$username = "admin"; // Tên đăng nhập MySQL
+$password = "Levannhat0904?"; // Mật khẩu MySQL
+$dbname = "employee_management"; // Tên cơ sở dữ liệu
 
 // Tạo kết nối
 if (!isset($conn)) {
